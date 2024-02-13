a4a31a4b446a2f0bf16586d0ad47ec724e396c2f
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-13 22:29:31 +0300

    fix banning by hhru

--- parc_vac.py
+++ parc_vac.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from icecream import ic
 import json
+import time
 
 
 
@@ -20,9 +21,19 @@ def parc_vac(url):
     name_vacancy = soup.find("h1", class_="bloko-header-section-1")
     salary = soup.find("span", class_="bloko-header-section-2 bloko-header-section-2_lite")
     experience = soup.find("p", class_="vacancy-description-list-item")
-    name_company = soup.find_all("span", class_="bloko-header-section-2 bloko-header-section-2_lite")[1]
-    desc = soup.find("div", class_="vacancy-description")
-    time_creation = soup.find("p", class_="vacancy-creation-time-redesigned")
+    try:
+        name_company = (soup.find_all("span", class_="bloko-header-section-2 bloko-header-section-2_lite")[1]).get_text().replace("\xa0", " ")
+    except:
+        name_company = ""    
+    try:
+        desc = soup.find("div", class_="vacancy-description").get_text().replace("О компании", "")
+    except:
+        desc = ""
+    try:        
+        time_creation = soup.find("p", class_="vacancy-creation-time-redesigned").get_text().replace("\xa0", " ")
+    except: 
+        time_creation = ""  
+          
     skills_divs = soup.find_all("div", class_="bloko-tag bloko-tag_inline")
     
     skills = []
@@ -34,9 +45,9 @@ def parc_vac(url):
         "name_vacancy": name_vacancy.get_text(),
         "salary": salary.get_text().replace("\xa0", " "),
         "experience": experience.get_text(),
-        "name_company": name_company.get_text().replace("\xa0", " "),
-        "description": desc.get_text().replace("О компании", ""),
-        "time_creation": time_creation.get_text().replace("\xa0", " "),
+        "name_company": name_company,
+        "description": desc,
+        "time_creation": time_creation,
         "skills": skills  
     }
     
@@ -52,6 +63,7 @@ with open(path, "r", encoding="utf-8") as fp:
 datas_out = []
 
 for data in datas:
+    time.sleep(3)
     try:
         result = {**data, **parc_vac(data["link"])}
         datas_out.append(result)  
@@ -59,8 +71,5 @@ for data in datas:
         with open(path_out, "w", encoding="utf-8") as fp:
             json.dump(datas_out, fp, ensure_ascii=False, indent=2)
     except Exception as e:
-        with open("err.txt", "a", encoding="utf-8") as fp:
-            fp.write(" field " + (str)(data["id_pair"][0]))  
-            fp.write(" spec " + (str)(data["id_pair"][1])) 
-            fp.write(" vac name " + (str)(data["link"]))  
-            fp.write(" vac name " + (str)(data["vacancy_name"]) + "\n")     
+        print(e, data["link"])
+        continue        
